017fcc5bd87e781d8ecc526bc2fbba13201c080d
凹凸曼 <543484525@qq.com>
2015-06-20 18:45:45 +0800

    Add ftherDay project

---
+++ assets/scripts/main.js
@@ -0,0 +1,72 @@
+// Created by sam mok 2015(Siso brand interactive team).
+
+"use strict";
+
+var app = {
+    create: function () {
+        //  create slider
+        app.mySwiper = new Swiper('.swiper-container', {
+            direction: 'vertical',
+            parallax:true,
+            noSwiping: false,
+            speed:500,
+            // init
+            onInit: function () {
+                $('.scene').eq(0).addClass('active');
+                $('.zindex2').show();
+            },
+
+            //  router
+            onTransitionEnd: function (swiper) {
+                var curIndex = swiper.activeIndex;
+
+                //  show bg
+                $('.scene').eq(curIndex).addClass('active')
+
+                //  show content
+                setTimeout(function () {
+                    $('.scene').eq(curIndex).addClass('active')
+                        .siblings('.scene').removeClass('active');
+                }, 250);
+            }
+        });
+
+        $('.btn_l').click(function(){
+            app.mySwiper.slideTo(0, 1000, false);
+            $('.scene').eq(0).addClass('active');
+        })
+
+        $('.btn_r').click(function(){
+            var dispaly = $('.alert').css("display");
+            if( dispaly == "none"){
+                $('.alert').fadeIn();
+                $('.scene06bg').fadeIn();
+            }else{
+                $('.alert').fadeOut();
+                $('.scene06bg').fadeOut();
+            }
+        })
+
+        $('.scene06bg').click(function(){
+            var dispaly = $('.alert').css("display");
+            if( dispaly != "none"){
+                $('.alert').fadeOut();
+                $('.scene06bg').fadeOut();
+            }
+        })
+
+        //  first time play BGM
+        var initSound = function () {
+            //  delay play
+            $('#audio')[0].play();
+
+            document.removeEventListener('touchstart', initSound, false);
+        };
+        document.addEventListener('touchstart', initSound, false);
+
+    }
+}
+$(function (){
+    // init app
+    app.create();
+});
